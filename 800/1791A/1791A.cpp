67de26639635e869a107cee11378a2e7c6bd62b1
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-03-19 23:37:52 +0600

    [1791A | 800] Accepted | Time: 31ms | Memory: 100KB

---
+++ 800/1791A/1791A.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+void solve() {
+
+  string s="codeforces";
+  char a;
+  cin>>a;
+  if(s.find(a)!=string::npos){
+    cout<<"YES"<<endl;
+  }
+  else cout<<"NO"<<endl;
+  
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
