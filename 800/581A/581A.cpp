4054f1690756d034bf95c781c72c17d11d8f06d4
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-03-21 23:43:03 +0600

    [581A | 800] Accepted | Time: 46ms | Memory: 100KB

---
+++ 800/581A/581A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+void solve() {
+  int a,b;
+  cin>>a>>b;
+  int x=min(a,b);
+  int k=max(a,b);
+  int y=(k-x)/2;
+  cout<<x<<" "<<y;
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  //cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
